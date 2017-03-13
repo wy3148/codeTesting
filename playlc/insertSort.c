80abd17761eb26f8198ce90a817217ccc4c95269
Ian <iwang@fonality.com>
2017-03-13 21:19:08 +1100

    put file into lc

--- playlc/insertSort.c
+++ playlc/insertSort.c
@@ -4,6 +4,31 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+//insert sort complexity  O(n) best, worst  O(n^2)
+
+
+//from wiki
+void insertion_sort(int arr[], int len) 
+{
+    int i, j;
+    int temp;
+    for (i = 1; i < len; i++) 
+    {
+        temp = arr[i]; 
+//與已排序的數逐一比較，大於temp時，該數向後移
+        j = i - 1;  
+// 如果将赋值放到下一行的for循环内, 会导致在第10行出现j未声明的错误
+        for (; j >= 0 && arr[j] > temp; j--) 
+       { //j循环到-1时，由于[[短路求值]](http://zh.wikipedia.org/wiki/短路求值)，不会运算array[-1]
+            arr[j + 1] = arr[j];
+        }
+    arr[j + 1] = temp; 
+    //被排序数放到正确的位置
+    }
+}
+
+
 int* insertSort(int* a, int size, int* returnSize){
 
     *returnSize = size;
@@ -22,6 +47,8 @@ int* insertSort(int* a, int size, int* returnSize){
             j++;
             res[j] = a[i];
         }else{
+
+            //tooooooo complicated
             int pos = 0;
             for( m =j; m >=0; m--){
 
